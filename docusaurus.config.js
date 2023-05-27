80fe11aebc3e7e5efd131666b256f5618b0d8c53
lkobylinski <lkobylinski@gmail.com>
2023-05-27 10:16:37 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,10 +11,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://clinquant-sunshine-786572.netlify.app',
+  url: 'https://www.korpusy.pl',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/korpusy/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
